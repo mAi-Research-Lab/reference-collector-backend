042b1fcdd144cbbb87e50f5eb50744fa850ad115
Yesarib <yesaribaris23@gmail.com>
2025-07-16 11:11:20 +0300

    feat: add FileService and FileController to ReferencesModule for enhanced file management capabilities

--- src/modules/references/references.module.ts
+++ src/modules/references/references.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ReferencesService } from './references.service';
 import { ReferencesController } from './references.controller';
+import { FileService } from './services/file.service';
+import { FileController } from './controller/file.controller';
 
 @Module({
-  providers: [ReferencesService],
-  controllers: [ReferencesController]
+  providers: [
+    ReferencesService,
+    FileService
+  ],
+  controllers: [
+    ReferencesController,
+    FileController
+  ]
 })
 export class ReferencesModule {}
