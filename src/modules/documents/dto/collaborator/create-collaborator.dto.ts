fe0f8be49ba88f45f4e18125e646c821a3447d04
Yesarib <yesaribaris23@gmail.com>
2025-07-23 13:02:23 +0300

    feat: update DocumentCollaboratorController to include documentId in invite and accept routes, modify service methods accordingly, and enhance CreateCollaboratorDto by removing documentId property

--- src/modules/documents/dto/collaborator/create-collaborator.dto.ts
+++ src/modules/documents/dto/collaborator/create-collaborator.dto.ts
@@ -3,11 +3,6 @@ import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 import { CollaboratorRoles } from "generated/prisma";
 
 export class CreateCollaboratorDto {
-    @ApiProperty({ example: '14e56bb0-ed2f-4567-bb07-a3b2649ed80d' })
-    @IsString()
-    @IsNotEmpty()
-    documentId: string;
-
     @ApiProperty({ example: 'example@example.com' })
     @IsString()
     @IsNotEmpty()
